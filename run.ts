05211293f2c5fd7371d983b8c048e3b4f4643ca2
Jiwen Bai <56228105@qq.com>
2023-11-15 11:09:56 +0800

    fix: bug

--- run.ts
+++ run.ts
@@ -3,16 +3,15 @@ import fs from 'fs'
 import path from 'path'
 import { execSync, spawn } from 'child_process'
 
-const repo = 'git@github.com:Volankey/vite-svg-resource-preview.git'
+const repo = 'https://github.com/Volankey/vite-svg-resource-preview.git'
 
 const git = simpleGit()
 
 async function main() {
   const args = process.argv.slice(2)
-  console.log('args', args)
   const configPath = path.join(args[0])
 
-  console.log('configPath', configPath)
+  console.log('vite config: ', configPath)
   const base = path.dirname(configPath)
   const feDir = path.join(base, '.find-svg')
   if (fs.existsSync(feDir)) {
@@ -42,22 +41,23 @@ export default defineConfig((ops) => {
   execSync('pnpm i',{
     cwd: feDir,
     stdio: 'inherit',
+    shell: true,
+    env: { ...process.env, FORCE_COLOR: true }
   })
   // run vite build
   console.log('base',base)
   const ls = spawn('npx', ['vite', 'build', '-c', path.resolve(__dirname, './vite.config.ts')], {
     cwd: base,
-  })
-  ls.stdout.on('data', (data) => {
-    console.log(`[find svg]: ${data}`)
-  })
-  ls.stderr.on('data', (data) => {
-    console.error(`[find svg]: ${data}`)
+    stdio: 'inherit',
+    shell: true,
+    env: { ...process.env, FORCE_COLOR: true }
   })
   ls.on('exit', () => {
     execSync('npx vite dev', {
       cwd: feDir,
       stdio: 'inherit',
+      shell: true,
+      env: { ...process.env, FORCE_COLOR: true }
     })
   })
 }
